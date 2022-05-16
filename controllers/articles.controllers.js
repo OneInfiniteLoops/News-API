c5494ee9880e81885535043037a5ecea85906df6
1-Infiniteloop <skjyeung@gmail.com>
2022-05-16 15:25:12 +0100

    add catch error and Promise reject to controller and model respectively, rename functions to getArticleByID and fetchArticlebyID respectively, and pass error handling test

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,7 +1,12 @@
 const { fetchArticleByID } = require("../models/articles.models");
 
 exports.getArticleByID = (req, res, next) => {
-  fetchArticleByID(req.params.article_id).then((article) => {
-    res.status(200).send({ article });
-  });
+  fetchArticleByID(req.params.article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
