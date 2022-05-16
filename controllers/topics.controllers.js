617ff54ad93108e6f7afcdbb64dc891385457aaa
1-Infiniteloop <skjyeung@gmail.com>
2022-05-16 13:04:52 +0100

    remove console.log

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -2,7 +2,6 @@ const { fetchTopics } = require("../models/topics.models");
 
 exports.getTopics = (req, res, next) => {
   fetchTopics().then((topics) => {
-    // console.log({ topics }, "FROM CONTROLLER");
     res.status(200).send({ topics });
   });
 };
--- models/topics.models.js
+++ models/topics.models.js
@@ -2,7 +2,6 @@ const db = require("../db/connection");
 
 exports.fetchTopics = () => {
   return db.query(`SELECT * FROM topics`).then((topics) => {
-    // console.log(topics, "FROM MODELS");
     return topics.rows;
   });
 };
