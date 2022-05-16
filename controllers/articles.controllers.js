62ea7b657fb48b8fff41f60b76b0618fce1d065a
1-Infiniteloop <skjyeung@gmail.com>
2022-05-16 15:10:22 +0100

    add getArticleByID and fetchArticleByID with export and require and pass the corresponding tests

---
+++ controllers/articles.controllers.js
@@ -0,0 +1,7 @@
+const { fetchArticleByID } = require("../models/articles.models");
+
+exports.getArticleByID = (req, res, next) => {
+  fetchArticleByID(req.params.article_id).then((article) => {
+    res.status(200).send({ article });
+  });
+};
---
+++ models/articles.models.js
@@ -0,0 +1,10 @@
+const db = require("../db/connection");
+
+exports.fetchArticleByID = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then((article) => {
+      console.log(article.rows[0]);
+      return article.rows[0];
+    });
+};
