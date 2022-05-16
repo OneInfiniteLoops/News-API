e2030589a9c0fe75885f59a7691691e9ce4c5dc4
1-Infiniteloop <skjyeung@gmail.com>
2022-05-16 16:22:20 +0100

    edit labelling comments for error handlers on app.js

--- app.js
+++ app.js
@@ -10,24 +10,24 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleByID);
 
-//PSQL Error Handler: 400 – Bad Request for getArticleByID
+//400 – Bad Request (PSQL error)
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ message: "Bad Request" });
   } else next(err);
 });
 
-//Error Handler: 404 – Article not found error
+//404 – Path not found
 app.use("/*", (req, res, next) => {
   res.status(404).send({ message: "Requested URL not found" });
 });
 
-//Customer Error Handler for getArticleByID (404 Error)
+//404 – Article not found
 app.use((err, req, res, next) => {
   res.status(err.status).send({ message: err.message });
 });
 
-//Error Handler: 500 – Internal Server Error
+//500 – Internal Server Error
 app.use((err, req, res, next) => {
   res.status(500).send({ message: "Internal Server Error" });
 });
