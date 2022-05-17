53667be5b40aca786668639417967ca3b177785e
1-Infiniteloop <skjyeung@gmail.com>
2022-05-17 15:45:11 +0100

    make changes to db query SQL code to retrieve comment_count in new column

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,7 +2,10 @@ const db = require("../db/connection");
 
 exports.fetchArticleByID = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .query(
+      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
+      [article_id]
+    )
     .then((article) => {
       if (article.rows.length === 0) {
         return Promise.reject({
@@ -14,6 +17,7 @@ exports.fetchArticleByID = (article_id) => {
       }
     });
 };
+//`SELECT * FROM articles WHERE article_id = $1`
 
 exports.updateVotesOfArticleByID = (articleId, newVote) => {
   return db
