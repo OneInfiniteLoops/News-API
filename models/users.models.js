0f8d1b630bb5db405ea94910fca9ab754773676e
1-Infiniteloop <skjyeung@gmail.com>
2022-05-17 14:21:16 +0100

    refactor fetchUsers to select from username column only

--- models/users.models.js
+++ models/users.models.js
@@ -1,12 +1,7 @@
 const db = require("../db/connection");
 
 exports.fetchUsers = () => {
-  return db.query(`SELECT * FROM users`).then((users) => {
-    usersArray = users.rows;
-    const usernameArray = usersArray.map((user) => {
-      copyUsersArray = { ...usersArray };
-      return { username: user.username };
-    });
-    return usernameArray;
+  return db.query(`SELECT username FROM users`).then((usernames) => {
+    return usernames.rows;
   });
 };
