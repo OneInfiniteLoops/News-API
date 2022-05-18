3d8a4cf20eff1ad3042db72c135a94a13743671c
1-Infiniteloop <skjyeung@gmail.com>
2022-05-18 14:51:10 +0100

    add Promise reject to throw 404 error from fetchCommentsById if article does not exist and catch err in getCommentsById

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,7 +2,12 @@ const { fetchCommentsByArticleID } = require("../models/comments.models");
 
 exports.getCommentsByArticleID = (req, res, next) => {
   const articleId = req.params.article_id;
-  fetchCommentsByArticleID(articleId).then((comments) => {
-    res.status(200).send({ comments });
-  });
+  fetchCommentsByArticleID(articleId)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
--- models/comments.models.js
+++ models/comments.models.js
@@ -4,6 +4,13 @@ exports.fetchCommentsByArticleID = (articleId) => {
   return db
     .query(`SELECT * FROM comments WHERE article_id = $1`, [articleId])
     .then((comments) => {
-      return comments.rows;
+      if (!comments.rows.length) {
+        return Promise.reject({
+          status: 404,
+          message: "Comments and article does not exist",
+        });
+      } else {
+        return comments.rows;
+      }
     });
 };
