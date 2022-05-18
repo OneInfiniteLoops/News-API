14b6a704567d2bc442eb4fc4e176be2d583f6782
1-Infiniteloop <skjyeung@gmail.com>
2022-05-18 16:28:41 +0100

    add additional then block in fetchCommentsByArticleID to check for existence of article, and to return empty array of comments if article_id valid and exists but no comments for  the given article exist

--- models/comments.models.js
+++ models/comments.models.js
@@ -2,15 +2,20 @@ const db = require("../db/connection");
 
 exports.fetchCommentsByArticleID = (articleId) => {
   return db
-    .query(`SELECT * FROM comments WHERE article_id = $1`, [articleId])
-    .then((comments) => {
-      if (!comments.rows.length) {
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+    .then((article) => {
+      if (!article.rows.length) {
         return Promise.reject({
           status: 404,
           message: "Comments and article does not exist",
         });
-      } else {
-        return comments.rows;
-      }
+      } else return article.rows[0].article_id;
+    })
+    .then((articleId) => {
+      return db
+        .query(`SELECT * FROM comments WHERE article_id = $1`, [articleId])
+        .then((comments) => {
+          return comments.rows;
+        });
     });
 };
